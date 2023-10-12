2597a0174737be9b24b2dc907b9af78cbbc352ff
Malachi9311 <ntuthuko93dlamini@gmail.com>
2023-10-12 19:24:44 +0200

    clean ups

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const multer = require('multer');
 const bodyParser = require('body-parser');
 
 // connectDB
