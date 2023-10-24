850f87e15274b0e364a34b5c7db6356ab067dce2
Malachi9311 <ntuthuko93dlamini@gmail.com>
2023-10-24 18:07:53 +0200

    simple welcome page set up

--- api/app.js
+++ api/app.js
@@ -4,15 +4,16 @@ const app = express();
 // Middleware configuration
 const swaggerui = require('swagger-ui-express');
 const authorize = require('./middleware/Authorize');
-const swag = require('./middleware/swaggerUI');
+const swag = require('./utils/swaggerUI');
 // Connecting to the database
 const connectDB = require('./db/connect');
 // Routes
 const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
+const options = require('./utils/staticOptions');
 // Router Middleware
+app.use(express.static('public', options));
 app.use(express.json());
-app.get('/', (req, res) => res.send('Welcome to my social media app API'));
 app.use('/api/v1/', userRouter);
 app.use('/api/v1/', authorize, postRouter);
 app.use('/docs', swaggerui.serve, swaggerui.setup(swag));
